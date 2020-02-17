0c6f85ca2e4e66a6293021f34047646d46ab88c0
Eryk Napierala <eryk.piast@gmail.com>
2020-02-17 22:18:22 +0100

    Add ESLint with lint-staged and husky

--- src/index.jsx
+++ src/index.jsx
@@ -2,31 +2,31 @@ import React from 'react';
 import { AutoSizer, List } from 'react-virtualized';
 import { createElement } from 'react-syntax-highlighter';
 
-function rowRenderer({ rows, stylesheet, useInlineStyles }, { index, key, style }) {
-  return createElement({
-      node: rows[index],
-      stylesheet,
-      style,
-      useInlineStyles,
-      key
+function rowRenderer({ rows, stylesheet, useInlineStyles }) {
+  return ({ index, key, style }) => createElement({
+    node: rows[index],
+    stylesheet,
+    style,
+    useInlineStyles,
+    key,
   });
 }
 
 export default function virtualizedRenderer({ overscanRowCount = 10, rowHeight = 15 } = {}) {
   return ({ rows, stylesheet, useInlineStyles }) => (
-    <div style={{height: "100%"}}>
+    <div style={{ height: '100%' }}>
       <AutoSizer>
         {({ height, width }) => (
           <List
             height={height}
             width={width}
             rowHeight={rowHeight}
-            rowRenderer={rowRenderer.bind(null, { rows, stylesheet, useInlineStyles })}
+            rowRenderer={rowRenderer({ rows, stylesheet, useInlineStyles })}
             rowCount={rows.length}
             overscanRowCount={overscanRowCount}
           />
         )}
       </AutoSizer>
     </div>
-  )
+  );
 }
